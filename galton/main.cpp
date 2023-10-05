6cdf34cefd8be8f8ff539d2c9c1e99eb14f98d4d
Trinity Slifer <nicola.dagosta2@gmail.com>
2023-10-05 11:44:06 +0200

    pushback

--- galton/main.cpp
+++ galton/main.cpp
@@ -15,12 +15,10 @@ for (size_t i = 0; i < j; i++)
 return k;
 };
 
-
 Galton::Galton(std::vector<int> mat, int col) : matrix_{mat}, col_{col} {};
 std::vector<int> Galton::getMatrix() const {return matrix_;};
 int Galton::getCol() const {return col_;}
-int Galton::Get_ij(int i, int j) {if (j < col_)
-{
+int Galton::Get_ij(int i, int j) {if (j < col_){
     return matrix_[Sum(j) + i];
 }else{
 return matrix_[Sum(col_) + (j - col_)* col_ + i];
@@ -32,6 +30,10 @@ int Simulation::sgetCol() const { return col_; }
 void Simulation::evolve(Galton g){
 std::vector<int> mat;
 mat.reserve(g.getMatrix().size());
+mat.push_back(number_);
+for (size_t i = 1; i < g.getMatrix().size(); i++) {
+  mat.push_back(0);
+}
 for (size_t w = 0; w < g.getMatrix().size() ; w++)
 {
   int row = (g.getMatrix().size() - Sum(g.getCol())) / (Sum(g.getCol())) +
@@ -44,12 +46,12 @@ for (size_t w = 0; w < g.getMatrix().size() ; w++)
           for (size_t k = 0; k < mat[Sum(i) + j]; k++) {
             double x = gRandom->Rndm();
              if (x <= g.Get_ij(j, i)/2) {
-               mat[Sum(i) + i + j]+=1; 
+               mat[Sum(i) + i + j] += 1; 
              }else{
                mat[Sum(i) + i + j + 1] += 1;
              }
-            mat[Sum(i)+j]=0;
           }
+          mat[Sum(i) + j] = 0;
         }
     }
   }
